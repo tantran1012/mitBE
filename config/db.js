466eebfa8450bf8c91093e62fe757400718d5816
tantran1012 <57386469+tantran1012@users.noreply.github.com>
2022-10-26 18:49:47 +0700

    add DB

--- config/db.js
+++ config/db.js
@@ -1,15 +1,18 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 async function connect() {
-
-    await mongoose.connect("mongodb+srv://mitadmin:mitadmin123@mit.v0hxaqx.mongodb.net/mitDB?retryWrites=true&w=majority", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }).then(() => {
-        console.log("Database connected");
-    }).catch((error) => {
-        console.log("Error connecting to database");
-    });
+    await mongoose
+        .connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        .then(() => {
+            console.log("Database connected");
+        })
+        .catch((error) => {
+            console.log("Error connecting to database");
+        });
 }
 
 module.exports = { connect };
